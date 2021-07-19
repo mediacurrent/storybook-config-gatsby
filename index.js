ce554628989d1aed0012c950d301286c144b6bfc
Grayson Hicks <graysonhicks@gmail.com>
2021-07-19 13:42:18 -0400

    webpack 5 compatibility

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 module.exports = (config) => {
-  config.node = { fs: 'empty', module: 'empty' };
   // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
-  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
+  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
 
   // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
-  config.module.rules[0].use[0].loader = require.resolve('babel-loader');
+  config.module.rules[0].use[0].loader = require.resolve('babel-loader')
 
   // use @babel/preset-react for JSX and env (instead of staged presets)
   config.module.rules[0].use[0].options.presets = [
     require.resolve('@babel/preset-react'),
     require.resolve('@babel/preset-env')
-  ];
+  ]
 
   // use @babel/plugin-proposal-class-properties for class arrow functions
   config.module.rules[0].use[0].options.plugins = [
@@ -20,39 +19,40 @@ module.exports = (config) => {
     // use @babel/plugin-proposal-optional-chaining to allow for js optional chaining in Storybook
     require.resolve('@babel/plugin-proposal-optional-chaining'),
     require.resolve('@loadable/babel-plugin')
-  ];
+  ]
 
   // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
-  config.resolve.mainFields = ['browser', 'module', 'main'];
+  config.resolve.mainFields = ['browser', 'module', 'main']
 
   // Add SCSS Modules support.
   config.module.rules.push({
     test: /\.scss$/,
     rules: [
-      require.resolve('style-loader'),
+      { loader: require.resolve('style-loader') },
       {
         loader: require.resolve('css-loader'),
         options: {
           importLoaders: 1,
-          modules: true,
-          localIdentName: '[name]__[local]___[hash:base64:5]'
+          modules: {
+            localIdentName: '[name]__[local]___[hash:base64:5]'
+          }
         }
       },
-      require.resolve('sass-loader')
+      { loader: require.resolve('sass-loader') }
     ]
-  });
+  })
 
   // Exclude SVGs from the default config so we can use svg-react-loader.
   const fileLoaderRule = config.module.rules.find(
     (rule) => rule.test && rule.test.test('.svg')
-  );
-  fileLoaderRule.exclude = /\.svg$/;
+  )
+  fileLoaderRule.exclude = /\.svg$/
 
   config.module.rules.push({
     test: /\.svg$/,
     enforce: 'pre',
     loader: require.resolve('svg-react-loader')
-  });
+  })
 
-  return config;
+  return config
 }
